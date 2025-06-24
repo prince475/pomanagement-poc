41bf77de1641fa368e9c4ae7f5a9dd77a0c8803c
prince475 <danielmwai475@gmail.com>
2025-06-24 07:49:27 +0300

    Cleaning our code structure for a more cleaner readable code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+// src/App.jsx
+import Navbar from "./components/Navbar";
+import DashboardMain from "./components/DashboardMain";
+import Footer from "./components/Footer";
+
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-xl shadow-lg text-center">
-        <h1 className="text-3xl font-bold text-blue-600">Welcome to PO Management POC</h1>
-        <p className="mt-4 text-gray-600">Tailwind CSS is fully set up and ready to go!</p>
-        <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-          Get Started
-        </button>
-      </div>
-    </div>
+    <>
+      <main className="overflow-x-hidden">
+        <Navbar />
+        <DashboardMain />
+        <Footer/>
+      </main>
+    </>
   );
 }
